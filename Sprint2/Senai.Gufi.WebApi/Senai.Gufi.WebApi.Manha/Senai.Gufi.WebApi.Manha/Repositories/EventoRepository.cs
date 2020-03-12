d2ba076b4a4611ef266f7b15f70af9692cd3d466
Carlos Eduardo Barbosa Gonçalves <43442346851@info.local>
2020-03-12 11:29:21 -0300

    Inicio API Gufi

---
+++ Sprint2/Senai.Gufi.WebApi/Senai.Gufi.WebApi.Manha/Senai.Gufi.WebApi.Manha/Repositories/EventoRepository.cs
@@ -0,0 +1,45 @@
+﻿using Senai.Gufi.WebApi.Manha.Domains;
+using Senai.Gufi.WebApi.Manha.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Gufi.WebApi.Manha.Repositories
+{
+    public class EventoRepository : IEventoRepository
+    {
+        GufiContext ctx = new GufiContext();
+
+        public void Atualizar(int id, Evento eventoAtualizado)
+        {
+            Evento eventoBuscado = ctx.Evento.Find(id);
+
+            eventoBuscado.NomeEvento = eventoAtualizado.NomeEvento;
+
+            ctx.Evento.Update(eventoBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Evento> BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Evento novoEvento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Evento> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
